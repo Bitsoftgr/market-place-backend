f5e55ee5ee62b4af9adbf1f19ffc2311d1788e1f
Muhammadali Saidov <zamirahamidova47@gmail.com>
2025-10-26 07:07:45 +0500

    profiel  controller added and routers is ready to start so middlware is for checking user token exist

---
+++ middlware/authenTokenCheck.js
@@ -0,0 +1,23 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken"
+import ENV from "../lib/ENV";
+const authenTokenCheck=async(req,res,next)=>{
+    try {
+        const token=res.cookie.token
+        if(!token)
+            return res.status(404).json({warning:"Token is not exists"})
+
+
+        jwt.verify(token,ENV.JWT_SECRET,(err,user)=>{
+            if(err)
+                return res.status(403).json({warning:"Token is modified"})
+            req.user=user
+            next()
+        })
+    } catch (error) {
+           console.log("Error ocured while  checking token"+error.message)
+        res.status(500).json({error:"Internal server error"})
+    }
+}
+
+export default authenTokenCheck
